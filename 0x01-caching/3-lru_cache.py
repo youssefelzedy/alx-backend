5a5cd24d86631b072a16b65b4316a3d12505db9c
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-26 00:13:45 +0300

    task 3

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""LRU BaseCaching module
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''Caching system
+    '''
+
+    def __init__(self):
+        """ Initiliaze
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        '''Add an item in the cache
+        '''
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.queue[0]))
+                del self.cache_data[self.queue[0]]
+                del self.queue[0]
+            if key in self.queue:
+                del self.queue[self.queue.index(key)]
+            self.queue.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''Get an item by key
+        '''
+        if key in self.cache_data.keys():
+            return self.cache_data[key]
+        else:
+            return None
