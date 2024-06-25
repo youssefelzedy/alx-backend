9c9b35c4222b0c311f5fbcad35cdb8fe9e91ce75
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-06-26 00:01:53 +0300

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,15 +18,16 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         '''Add an item in the cache
         '''
-        lens = len(self.cache_data)
-        if key not in self.cache_data and lens >= BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.queue[0]))
-            del self.cache_data[self.queue[0]]
-            del self.queue[0]
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.queue[0]))
+                del self.cache_data[self.queue[0]]
+                del self.queue[0]
             self.queue.append(key)
             self.cache_data[key] = item
-        else:
-            pass
 
     def get(self, key):
         '''Get an item by key
