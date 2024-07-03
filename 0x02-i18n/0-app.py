896f2687ad281e12708bea1249b3118a22d3b95b
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-03 10:45:56 +0300

    0 task

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,8 +7,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/', method=['GET'], strict_slashes=False)
+def hello_world() -> str:
     '''Hello World!'''
     return render_template('0-index.html')
 
