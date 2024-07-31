a63337c35e8fa6183801c8cf8b19efaf5f97b877
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-31 11:48:05 +0300

    final task

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -1,10 +1,11 @@
-import { createClient } from "redis";
+import redis from 'redis';
+
 const client = redis.createClient();
 
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
 
 client.on('connect', () => {
-    console.log('Redis client connected to the server');
-});
-client.on('error', (err) => {
-    console.log(`Redis client not connected to the server: ${error}`);
+  console.log('Redis client connected to the server');
 });
